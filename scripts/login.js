c74c38cb90f9516b5d6b7d16a02ec3c3c559f4fd
Lucas Cantoni <lukcnt@outlook.com>
2022-11-06 02:25:04 -0300

    Create a function that compare the login and password data to allow or deny the access

--- scripts/login.js
+++ scripts/login.js
@@ -2,6 +2,16 @@ const usuario = document.querySelector("#usuario");
 const senha = document.querySelector("#senha");
 const botaoAcessar = document.querySelector("#botao-login");
 
+function compararDados(dadosJson){
+    let acessoLiberado = false;
+    for(let i = 0; i < dadosJson.users.length; i++){
+        if(usuario.value == dadosJson.users[i].user && senha.value == dadosJson.users[i].pws){
+            acessoLiberado = true;
+        }
+    }
+    return acessoLiberado;
+};
+
 async function buscarJson() {
     let url = "./json/usuario.json";
     let dadosFetch = await fetch(url);
