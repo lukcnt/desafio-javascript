3efbe0b6361c4bf52f573bfa1d3b94d67aa645d5
Lucas Cantoni <lukcnt@outlook.com>
2022-11-06 02:11:19 -0300

    Create an event listener to when the acessar button be clicked call a function

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,7 @@
 const usuario = document.querySelector("#usuario");
 const senha = document.querySelector("#senha");
 const botaoAcessar = document.querySelector("#botao-login");
+
+botaoAcessar.addEventListener("click", function(){
+    buscar();
+});
