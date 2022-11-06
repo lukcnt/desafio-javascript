2c6a0ef5b935aabe3b0a5b098281e4555adbee97
Lucas Cantoni <lukcnt@outlook.com>
2022-11-06 02:21:35 -0300

    Create a function that fetch the data of an external json file

--- scripts/login.js
+++ scripts/login.js
@@ -2,6 +2,12 @@ const usuario = document.querySelector("#usuario");
 const senha = document.querySelector("#senha");
 const botaoAcessar = document.querySelector("#botao-login");
 
+async function buscarJson() {
+    let url = "./json/usuario.json";
+    let dadosFetch = await fetch(url);
+    let dadosJson = await dadosFetch.json();
+};
+
 botaoAcessar.addEventListener("click", function(){
     buscarJson();
 });
