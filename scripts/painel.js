17eeba04cf29c9a5f8da6c3d51caee3de332bb7e
Lucas Cantoni <lukcnt@outlook.com>
2022-11-07 21:52:29 -0300

    Refactoring the painel javascript code

--- scripts/painel.js
+++ scripts/painel.js
@@ -1,37 +1,31 @@
 import { clientes } from "../scripts/clientes.js";
 import { produtos } from "../scripts/produtos.js";
 
-function fecharJanela(className, event) {
-    let list = document.querySelectorAll(className);
-    for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener(event, function() {
-            document.querySelector("#janela-clientes").style.display = "none";
+let fecharJanela = document.querySelectorAll(".fechar");
+let abrirJanela = document.querySelectorAll("#menu li");
+
+for(let i = 0; i < fecharJanela.length; i++) {
+    fecharJanela[i].addEventListener("click", function() {
+        document.querySelector("#janela-clientes").style.display = "none";
+        document.querySelector("#janela-produtos").style.display = "none";
+        document.querySelector("#janela-pedidos").style.display = "none";
+    });
+}
+
+for(let i = 0; i < abrirJanela.length; i++) {
+    abrirJanela[i].addEventListener("click", function(){
+        if(abrirJanela[i].id == "menu-clientes") {
+            document.querySelector("#janela-clientes").style.display = "flex";
             document.querySelector("#janela-produtos").style.display = "none";
             document.querySelector("#janela-pedidos").style.display = "none";
-        });
-    }
+        } else if (abrirJanela[i].id == "menu-produtos") {
+            document.querySelector("#janela-produtos").style.display = "flex";
+            document.querySelector("#janela-clientes").style.display = "none";
+            document.querySelector("#janela-pedidos").style.display = "none";
+        } else {
+            document.querySelector("#janela-pedidos").style.display = "flex";
+            document.querySelector("#janela-clientes").style.display = "none";
+            document.querySelector("#janela-produtos").style.display = "none";
+        }
+    });
 }
-
-function abrirJanela(idName, event) {
-    let list = document.querySelectorAll(idName);
-    for (let i = 0; i < list.length; i++) {
-        list[i].addEventListener(event, function(){
-            if(list[i].id == "menu-clientes") {
-                document.querySelector("#janela-clientes").style.display = "flex";
-                document.querySelector("#janela-produtos").style.display = "none";
-                document.querySelector("#janela-pedidos").style.display = "none";
-            } else if (list[i].id == "menu-produtos") {
-                document.querySelector("#janela-produtos").style.display = "flex";
-                document.querySelector("#janela-clientes").style.display = "none";
-                document.querySelector("#janela-pedidos").style.display = "none";
-            } else {
-                document.querySelector("#janela-pedidos").style.display = "flex";
-                document.querySelector("#janela-clientes").style.display = "none";
-                document.querySelector("#janela-produtos").style.display = "none";
-            }
-        });
-    }
-}
-
-abrirJanela("#menu li", "click");
-fecharJanela('.fechar', 'click');
